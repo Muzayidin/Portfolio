8c2a027c82066dd5cc2548e04ea2bf688f34901d
Muzayin <96785254+Muzayidin@users.noreply.github.com>
2023-05-03 00:39:18 +0700

    first

---
+++ script.js
@@ -0,0 +1,23 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("is-active");
+  menu.classList.toggle("menu-active");
+});
+
+window.addEventListener("scroll", () => {
+  hamburger.classList.remove("is-active");
+  menu.classList.remove("menu-active");
+});
+
+const loader = document.querySelector(".loader");
+
+function loaderActive() {
+  loader.classList.add("loader-active");
+}
+function loaderActiveTime() {
+  setInterval(loaderActive, 200);
+}
+
+window.onload = loaderActiveTime();
